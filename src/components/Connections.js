f32382d110c8843d6132f40e3e3a084a82dd13a7
Omri Gazitt <ogazitt@gmail.com>
2019-11-12 16:29:16 -0800

    Connections control now expects connections to be constructed by the API

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -5,18 +5,6 @@ import CardDeck from 'react-bootstrap/CardDeck';
 
 import { useConnections } from '../utils/connections';
 
-const connectionList = {
-  google: {
-    image: 'google-logo.png'
-  },
-  facebook: {
-    image: 'facebook-logo.png'
-  },
-  instagram: {
-    image: 'instagram-logo.png'
-  }
-};
-
 const Connections = () => {
   const { connections, loaded } = useConnections();
 
@@ -27,16 +15,16 @@ const Connections = () => {
   return (
     <CardDeck>
     {
-      Object.keys(connectionList).map((index, key) => {
-        const connected = connections[index];
+      connections.map((connection, key) => {
+        const connected = connection.connected;
         return (
           <Card 
             key={key} 
             border={ connected ? 'success' : 'secondary' }
             style={{ maxWidth: '150px', textAlign: 'center' }}>
-            <center><Card.Img variant="top" src={connectionList[index].image} style={{ width: '8rem', marginTop: '10px' }}/></center>
+            <center><Card.Img variant="top" src={connection.image} style={{ width: '8rem', marginTop: '10px' }}/></center>
             <Card.Body>
-              <Card.Title className="text-center">{index}</Card.Title>
+              <Card.Title className="text-center">{connection.provider}</Card.Title>
               <Button variant={ connected ? 'danger' : 'primary' }>
                 { connected ? 'Disconnect' : 'Connect'}
               </Button>
