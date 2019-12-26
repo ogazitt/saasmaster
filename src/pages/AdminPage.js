1eca4d7ff9c115459611cc02ce16b4e0accf2cc0
Omri Gazitt <ogazitt@gmail.com>
2019-12-26 13:03:01 -0800

    added profile data to AdminPage, built a finished ProfilePage

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
+import Highlight from '../components/Highlight'
 import { useAuth0 } from '../utils/react-auth0-wrapper'
 import { useConnections } from '../utils/connections'
 import { useMetadata } from '../utils/metadata'
+import { get } from '../utils/api'
+
 
 const AdminPage = () => {
 
-  const { isAdmin, impersonatedUser, setImpersonatedUser } = useAuth0();
+  const { isAdmin, impersonatedUser, setImpersonatedUser, getTokenSilently } = useAuth0();
   const { loadConnections } = useConnections();
   const { loadMetadata } = useMetadata();
-  const [user, setUser] = useState(impersonatedUser || "");
+  const [userId, setUserId] = useState(impersonatedUser || "");
+  const [profile, setProfile] = useState({});
 
   // use an effect to re-load connections and metadata if the impersonated user has been updated  
   useEffect(() => {
@@ -20,6 +24,25 @@ const AdminPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [impersonatedUser]);
 
+  // use an effect to load the profile information for the currently impersonated user
+  useEffect(() => {
+    async function loadProfile() {
+      const token = await getTokenSilently();
+      const [response, error] = await get(token, 'profile', 
+        impersonatedUser ?  { impersonatedUser: impersonatedUser } : {});
+
+      if (error || !response.ok) {
+        setProfile({ message: 'error retrieving profile'});
+        return;
+      }
+
+      const responseData = await response.json();
+      setProfile(responseData);
+    }
+    loadProfile();
+  }, [impersonatedUser]);
+
+  // display an error if current user isn't an admin
   if (!isAdmin) {
     return (
       <div className="provider-header">
@@ -34,25 +57,37 @@ const AdminPage = () => {
   }
 
   return (
-    <div>
+    <div style={{ marginLeft: 20 }}>
       <div className="provider-header">
         <h4 className="provider-title">Admin</h4>
       </div>
 
-      <div style={{ margin: 20, width: 450 }}>
+      <div style={{ margin: 20, marginTop: 0, width: 450 }}>
         <InputGroup className="mb-3">
           <InputGroup.Prepend>
             <InputGroup.Text id="basic-addon1">Impersonate: </InputGroup.Text>
           </InputGroup.Prepend>
-          <FormControl placeholder="username" value={user} onChange={(e) => setUser(e.target.value)}/>
+          <FormControl placeholder="username" value={userId} onChange={(e) => setUserId(e.target.value)}/>
         </InputGroup>
 
         <div style={{ display: 'flex' }}>
-          <Button style={{ marginRight: 20}} onClick={() => impersonate(user)}>Impersonate</Button>
-          <Button style={{ marginRight: 20}} onClick={() => { setUser(""); impersonate(null)}}>Clear</Button>
+          <Button style={{ marginRight: 20}} onClick={() => impersonate(userId)}>Impersonate</Button>
+          <Button style={{ marginRight: 20}} onClick={() => { setUserId(""); impersonate(null)}}>Clear</Button>
         </div>
       </div>
-
+      <div style={{
+        padding: '1px 0 0 20px',
+        textAlign: 'left',
+        width: 'calc(100vw - 60px)'
+      }}>
+      { 
+        profile && 
+        <div>
+          <h5>Profile data</h5>
+          <Highlight>{JSON.stringify(profile, null, 2)}</Highlight> 
+        </div>
+      }
+      </div>
     </div>
   )
 }
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Loading from '../components/Loading'
-import Highlight from '../components/Highlight'
 import RefreshButton from '../components/RefreshButton'
+import { Form, Col, Row, Image, Card, CardDeck } from 'react-bootstrap'
 import { useAuth0 } from '../utils/react-auth0-wrapper'
+import { useConnections } from '../utils/connections'
 import { get } from '../utils/api'
 
 const ProfilePage = () => {
@@ -10,7 +11,8 @@ const ProfilePage = () => {
   const [loading, setLoading] = useState(false);
   const [loadedData, setLoadedData] = useState(false);
   const [refresh, setRefresh] = useState(false);
-  const { getTokenSilently, user, impersonatedUser } = useAuth0();
+  const { getTokenSilently, impersonatedUser } = useAuth0();
+  const { connections } = useConnections();
 
   // if in the middle of a loading loop, put up loading banner and bail
   if (loading && !refresh) {
@@ -46,6 +48,37 @@ const ProfilePage = () => {
     loadData();
   }
 
+  const name = profile && profile.name;
+  const email = profile && profile.email;
+  const picture = profile && profile.picture;
+  const lastLogin = profile && profile.last_login && new Date(profile.last_login).toLocaleString();
+
+  const connectionCards = profile && profile.identities && 
+        connections && connections.map && connections.map(connection => {
+    // set up some variables
+    const connected = connection.connected ? true : false;
+    const color = connected ? 'success' : 'danger';
+    const [providerTitle] = connection.provider.split('-');
+    const label = <i className={`fa fa-fw fa-${providerTitle} text-${color}`} style={{ fontSize: '1.2em' }} />;
+    const provider = profile.identities.find(p => p.provider === connection.provider);
+    console.log(provider)
+    const userName = provider && provider.profileData && provider.profileData.name || name;
+    const userPicture = provider && provider.profileData && provider.profileData.picture || picture;
+    const screenName = provider && provider.profileData && provider.profileData.screen_name;
+    const location = provider && provider.profileData && provider.profileData.location;
+
+    return {
+      connected,
+      color,
+      providerTitle,
+      label,
+      userName,
+      userPicture,
+      screenName,
+      location
+    }
+  });
+
   return(
     <div style={{
       marginLeft: 20,
@@ -58,10 +91,48 @@ const ProfilePage = () => {
         <h4 className="provider-title">Profile</h4>
       </div>
       { 
-        loadedData ? <Highlight>{JSON.stringify(profile, null, 2)}</Highlight> : <div/>
-      }
-      {
-        loadedData ? <Highlight>{JSON.stringify(user, null, 2)}</Highlight> : <div/>
+        loadedData && 
+        <div>
+          <Card>
+            <Form>
+              <Row>
+                <Col sm="9">
+                  <Card.Title style={{ margin: 20 }}>Name: {name}</Card.Title>
+                  <Card.Title style={{ margin: 20 }}>Email: {email}</Card.Title>
+                  <Card.Title style={{ margin: 20 }}>Last login: {lastLogin}</Card.Title>
+                </Col>
+                <Col sm="3">
+                  <Image src={picture} style={{ height: 120, width: 120, margin: 20 }} roundedCircle />
+                </Col>
+              </Row>
+            </Form> 
+          </Card> 
+
+          <CardDeck style={{ padding: 20 }}>
+          { 
+            connectionCards && connectionCards.map((connection, key) => {
+              console.log(connection)
+              return (
+                <Card 
+                  key={key} 
+                  className='mx-auto'
+                  border={connection.color}
+                  style={{ maxWidth: '200px', minWidth: '200px', minHeight: '150px', textAlign: 'center' }}>
+                  <Card.Header>{connection.label}</Card.Header>
+                  <Card.Body>
+                    <Card.Title>{ connection.connected && connection.userName }</Card.Title>
+                    { connection.connected && connection.userPicture && 
+                      <center><Card.Img variant="top" src={connection.userPicture} style={{ width: '6rem', margin: '10px' }}/></center>
+                    }
+                    { connection.connected && connection.screenName && <Card.Subtitle><p>@{connection.screenName}</p></Card.Subtitle> }
+                    { connection.connected && connection.location && <Card.Subtitle>{connection.location}</Card.Subtitle> }
+                  </Card.Body>
+                </Card>    
+              )
+            })
+          }
+          </CardDeck>               
+        </div>
       }
     </div>
   )
